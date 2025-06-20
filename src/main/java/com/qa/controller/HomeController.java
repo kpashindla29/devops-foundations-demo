de383733a8d1649d8804df51300a02086eb2e8a2
Kishore Pashindla <kishore.pashindla@gmail.com>
2025-06-20 19:55:52 +0100

    Home controller updated

--- src/main/java/com/qa/controller/HomeController.java
+++ src/main/java/com/qa/controller/HomeController.java
@@ -8,7 +8,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Welcome to DevOps Foundations!!";
+        return "Welcome to DevOps Foundations Demo!!";
     }
 
 }
